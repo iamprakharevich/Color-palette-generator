33f6ccf206281f76f21c2387cec5f59d6ce80303
Aleh Prakharevich <iamprakharevich@yandex.ru>
2024-04-17 10:58:03 +0300

    Defining global variables

--- index.js
+++ index.js
@@ -16,3 +16,13 @@
 ⣿⣿⣿⣿⣿⣿⣿Isn't that wonderful? ⣦⡈⣿⣿⣿⣿
 ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡈⣿⣿
 */
+
+// general params
+
+let container = document.querySelector(".container"),
+cells = document.querySelectorAll(".cell"),
+mode_field = document.querySelector(".mode"),
+color_field = document.getElementById("color"),
+chosen_mode = "", chosen_color = "", chosen_code = "",
+mode = ["monochrome", "monochrome-dark", "monochrome-light", "analogic", "complement", "analogic-complement", "triad", "quad"],
+url = ""

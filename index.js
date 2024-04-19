3e6f189f85f5bf46313e7063268442c45e758d1e
Aleh Prakharevich <iamprakharevich@yandex.ru>
2024-04-19 15:58:14 +0300

    Add side menu / index.html, style.css, index.js

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ cells = document.querySelectorAll(".cell"),
 mode_field = document.querySelector(".mode"),
 color_field = document.getElementById("color"),
 chosen_mode = "", chosen_color = "", chosen_code = "",
+menu = document.querySelector(".right-menu"),
+menu_btn = document.querySelector(".fa-bars"),
+menu_close = document.querySelector(".fa-xmark"),
 mode = ["monochrome", "monochrome-dark", "monochrome-light", "analogic", "complement", "analogic-complement", "triad", "quad"],
 url = ""
 
@@ -32,3 +35,11 @@ url = ""
 function updateAllParams() {
     cells = document.querySelectorAll(".cells")
 }
+
+menu_btn.addEventListener("click", ()=>{
+    menu.classList.toggle("_active")
+})
+
+menu_close.addEventListener("click", ()=>{
+    menu.classList.toggle("_active")
+})

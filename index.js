7b90536604dd57b0d6fd0e042d316e16a6c1c03e
Aleh Prakharevich <iamprakharevich@yandex.ru>
2024-04-29 09:21:37 +0300

    Shades create function and color code mode switch

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ chosen_mode = "", chosen_color = "", chosen_code = "",
 menu = document.querySelector(".right-menu"),
 menu_btn = document.querySelector(".fa-bars"),
 menu_close = document.querySelector(".fa-xmark"),
+shades_field = document.querySelectorAll(".shadesField"),
 colorCode = document.querySelectorAll(".colorCode"),
 colorname = document.querySelectorAll(".colorname"),
 colorTools = document.querySelectorAll(".colorTools"),
@@ -45,6 +46,7 @@ url = "", lockedId
 
 function updateAllParams() {
     cells = document.querySelectorAll(".cell")
+    shades_field = document.querySelectorAll(".shadesField")
     colorCode = document.querySelectorAll(".colorCode")
     colorname = document.querySelectorAll(".colorname")
     colorTools = document.querySelectorAll(".colorTools")
@@ -70,6 +72,11 @@ setInterval(()=>{
         element1.onclick = () => {
             localStorage.setItem(`${fetchedColor.colors[i].name.value}`, JSON.stringify(fetchedColor.colors[i]))
             fetchFavoriteColor()
+        }
+        const element2 = colorTools[i].childNodes[2];
+        element2.onclick = () => {
+            getShades(fetchedColor.colors[i].hsl,i)
+            console.log(i)
         }
         const element3 = colorTools[i].childNodes[3];
         element3.onclick = () => { //lock and unlock color generate
@@ -321,14 +328,14 @@ for (let i = 0; i < rm_cont_head.length; i++) {
     element.addEventListener("click",()=>{
         switch (i) {
             case 0:
-                rm_cont_head[i].classList.add("_rm_active")
-                rm_cont_head[i+1].classList.remove("_rm_active")
+                rm_cont_head[i].classList.add("_active_btn")
+                rm_cont_head[i+1].classList.remove("_active_btn")
                 rm_cont_cont[i].setAttribute("style","display:flex;")
                 rm_cont_cont[i+1].setAttribute("style","display:none;")
                 break;
             case 1:
-                rm_cont_head[i].classList.add("_rm_active")
-                rm_cont_head[i-1].classList.remove("_rm_active")
+                rm_cont_head[i].classList.add("_active_btn")
+                rm_cont_head[i-1].classList.remove("_active_btn")
                 rm_cont_cont[i].setAttribute("style","display:flex;")
                 rm_cont_cont[i-1].setAttribute("style","display:none;")
                 break;
@@ -337,6 +344,225 @@ for (let i = 0; i < rm_cont_head.length; i++) {
         }
     })
 }
+
+// +color shades generation
+
+async function getShades(color,i) {
+    colorTools[i].childNodes[2].classList.toggle("_active_btn")
+    shades_field[i].classList.toggle("_active_shades")
+    shades_field[i].innerHTML = ""
+    let shades = [], convertable = []
+    if (shades_field[i].classList.contains("_active_shades")) {
+        for (let i = 1; i < 20; i++) {
+            shades.push([color.h,`${color.s}%`,`${i*5}%`])
+            convertable.push([color.h,color.s,i*5])
+        }
+        for (let j = 0; j < shades.length; j++) {
+            var div = document.createElement('div')
+            div.className = "shade"
+            switch (codeSelector.value) {
+                case "hex":
+                    div.innerHTML = `<p>${hslToHex(convertable[j])}</p>`
+                    break;
+                case "rgb":
+                    div.innerHTML = `<p>rgb(${hslToRgb(convertable[j])})</p>`
+                    break;
+                case "hsl":
+                    div.innerHTML = `<p>hsl(${shades[j]})</p>`
+                    break;
+                case "cmyk":
+                    div.innerHTML = `<p>cmyk(${hslToCmyk(convertable[j])})</p>`
+                    break;
+                default:
+                    break;
+            }
+            if (j<10) {
+                div.setAttribute("style", `background-color: hsl(${shades[j]}); color: #fff`)
+                div.setAttribute("value", `${convertable[j]}`)
+            } else {
+                div.setAttribute("style", `background-color: hsl(${shades[j]}); color: #000`)
+                div.setAttribute("value", `${convertable[j]}`)
+            }
+            shades_field[i].prepend(div)
+        }
+    }
+}
+
+function hslToHex([h, s, l]) {
+    h /= 360;
+    s /= 100;
+    l /= 100;
+    let r, g, b;
+    if (s === 0) {
+      r = g = b = l;
+    } else {
+      const hue2rgb = (p, q, t) => {
+        if (t < 0) t += 1;
+        if (t > 1) t -= 1;
+        if (t < 1 / 6) return p + (q - p) * 6 * t;
+        if (t < 1 / 2) return q;
+        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+        return p;
+      };
+      const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      const p = 2 * l - q;
+      r = hue2rgb(p, q, h + 1 / 3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1 / 3);
+    }
+    const toHex = x => {
+      const hex = Math.round(x * 255).toString(16);
+      return hex.length === 1 ? '0' + hex : hex;
+    };
+    return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+  }
+  
+  function hslToRgb([h, s, l]) {
+      s /= 100;
+      l /= 100;
+      const k = n => (n + h / 30) % 12;
+      const a = s * Math.min(l, 1 - l);
+      const f = n =>
+        l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)));
+      return [Math.round(255 * f(0)), Math.round(255 * f(8)), Math.round(255 * f(4))]; 
+  }
+  
+  // hsl to cmyk
+  
+  function hslToCmyk([h, s, l]) {
+    // Convert HSL to RGB
+    let c = (1 - Math.abs(2 * l - 1)) * s;
+    let x = c * (1 - Math.abs((h / 60) % 2 - 1));
+    let m = l - c / 2;
+    let r, g, b;
+    if (h >= 0 && h < 60) {
+      r = c;
+      g = x;
+      b = 0;
+    } else if (h >= 60 && h < 120) {
+      r = x;
+      g = c;
+      b = 0;
+    } else if (h >= 120 && h < 180) {
+      r = 0;
+      g = c;
+      b = x;
+    } else if (h >= 180 && h < 240) {
+      r = 0;
+      g = x;
+      b = c;
+    } else if (h >= 240 && h < 300) {
+      r = x;
+      g = 0;
+      b = c;
+    } else {
+      r = c;
+      g = 0;
+      b = x;
+    }
+    r = (r + m) * 255;
+    g = (g + m) * 255;
+    b = (b + m) * 255;
+  
+    // Convert RGB to CMYK
+    let k = 1 - Math.max(r, g, b) / 255;
+    let cmyk = {
+      c: (1 - r / 255 - k) / (1 - k),
+      m: (1 - g / 255 - k) / (1 - k),
+      y: (1 - b / 255 - k) / (1 - k),
+      k: k
+    };
+    cmyk.c *= 100;
+    cmyk.m *= 100;
+    cmyk.y *= 100;
+    cmyk.k *= 100;
+  
+    return cmyk;
+  }
+  
+  // -colorShades
+
+// -color shades generation
+
+// +color models switch
+
+function toHEX() {
+    for (let i = 0; i < colorCode.length; i++) {
+        const element = colorCode[i];
+        element.innerHTML = fetchedColor.colors[i].hex.value
+    }
+    for (let i = 0; i < shades_field_active.length; i++) {
+        const element = shades_field_active[i];
+        for (let i = 0; i < element.childNodes.length; i++) {
+            const element2 = element.childNodes[i];
+            element2.innerHTML = `<p>${hslToHex(element2.getAttribute("value").split(","))}</p>`
+        }
+    }
+}
+
+function toRGB() {
+    for (let i = 0; i < colorCode.length; i++) {
+        const element = colorCode[i];
+        element.innerHTML = fetchedColor.colors[i].rgb.value
+    }
+    for (let i = 0; i < shades_field_active.length; i++) {
+        const element = shades_field_active[i];
+        for (let i = 0; i < element.childNodes.length; i++) {
+            const element2 = element.childNodes[i];
+            element2.innerHTML = `<p>rgb(${hslToRgb(element2.getAttribute("value").split(","))})</p>`
+        }
+    }
+}
+
+function toCMYK() {
+    for (let i = 0; i < colorCode.length; i++) {
+        const element = colorCode[i];
+        element.innerHTML = fetchedColor.colors[i].cmyk.value
+    }
+    for (let i = 0; i < shades_field_active.length; i++) {
+        const element = shades_field_active[i];
+        for (let i = 0; i < element.childNodes.length; i++) {
+            const element2 = element.childNodes[i];
+            element2.innerHTML = `<p>cmyk(${hslToCmyk(element2.getAttribute("value").split(","))})</p>`
+        }
+    }
+}
+
+function toHSL() {
+    for (let i = 0; i < colorCode.length; i++) {
+        const element = colorCode[i];
+        element.innerHTML = fetchedColor.colors[i].hsl.value
+        element.setAttribute("style","font-size: 2rem;")
+    }
+    for (let i = 0; i < shades_field_active.length; i++) {
+        const element = shades_field_active[i];
+        for (let i = 0; i < element.childNodes.length; i++) {
+            const element2 = element.childNodes[i];
+            element2.innerHTML = `<p>hsl(${hslToHsl(element2.getAttribute("value").split(","))})</p>`
+        }
+    }
+}
+
+function switchColorCode() {
+    switch (codeSelector.value) {
+        case "hex":
+            toHEX();
+            break;
+        case "rgb":
+            toRGB();
+            break;
+        case "hsl":
+            toHSL();
+            break;
+        case "cmyk":
+            toCMYK();
+            break;
+        default:
+            break;
+    }
+}
+
+// -color models switch
 
 //Hotkeys
 document.addEventListener('keypress', function(event) {
